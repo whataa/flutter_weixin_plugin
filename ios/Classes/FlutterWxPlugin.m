b3fabab970e62ee969d4d014917cf85aef86590d
linjiang <linjiang.me@gmail.com>
2019-05-27 21:05:04 +0800

    update nothing.

--- ios/Classes/FlutterWxPlugin.m
+++ ios/Classes/FlutterWxPlugin.m
@@ -3,18 +3,14 @@
 @implementation FlutterWxPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_wx"
+      methodChannelWithName:@"tech.linjiang.flutter.wx/plugin"
             binaryMessenger:[registrar messenger]];
   FlutterWxPlugin* instance = [[FlutterWxPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+
 }
 
 @end
